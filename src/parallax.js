1a0d29acf5f8aaa9f353f6d41b5db862cc7fd487
Ali Ghassemi <aghassemi@google.com>
2016-11-17 11:30:10 -0800

    a

--- src/parallax.js
+++ src/parallax.js
@@ -19,34 +19,69 @@ import {viewportForDoc} from './viewport';
 import {vsyncFor} from './vsync';
 
 export function installParallax(global) {
-  const elements = global.document.querySelectorAll('*[parallax]');
+  const parallaxElements = global.document.querySelectorAll('*[parallax]');
+  const fadeElements = global.document.querySelectorAll('*[fade]');
+  for (let i = 0; i < fadeElements.length; i++) {
+    st.setStyles(fadeElements[i], {
+      'opacity': 0,
+    });
+  }
+
   const viewport = viewportForDoc(global.document);
   const vsync = vsyncFor(global);
-  const state = {scrollTop: 0};
 
   viewport.onScroll(schedule);
 
-  function measure(state) {
-    state.scrollTop = viewport.getScrollTop();
-  }
+  function parallaxMutate() {
+    for (let i = 0; i < parallaxElements.length; i++) {
+      const element = parallaxElements[i];
+      const rec = element.getBoundingClientRect();
+      if (!isInView(rec, viewport)) {
+        continue;
+      };
 
-  function mutate(state) {
-    for (let i = 0; i < elements.length; i++) {
-      const element = elements[i];
       let factor = element.getAttribute('parallax');
       factor = factor ? parseFloat(factor) : 0.5;
-      const newY = (state.scrollTop * factor).toFixed(2);
+      const top = viewport.getScrollTop();
+      const newY = (top * factor).toFixed(2);
       st.setStyles(element, {
         transform: 'translate3d(0,' + newY + 'px,0)',
       });
     }
   }
 
+  function fadeInMutate() {
+    for (let i = 0; i < fadeElements.length; i++) {
+      const element = fadeElements[i];
+      const rec = element.getBoundingClientRect();
+      if (!isInView(rec, viewport) || element.ampSkipFade) {
+        continue;
+      };
+      const h = viewport.getHeight() / 2;
+      if (rec.top < h) {
+        st.setStyles(element, {
+          'opacity': 1,
+        });
+        element.ampSkipFade = true;
+        continue;
+      }
+      const factor = (1 - Math.abs(((h - rec.top) / h)).toFixed(2));
+
+      st.setStyles(element, {
+        'opacity': factor,
+      });
+    }
+  }
+
   function schedule() {
-    vsync.run({
-      measure,
-      mutate,
-    }, state);
+    vsync.mutate(function() {
+      parallaxMutate();
+      fadeInMutate();
+    });
+  }
+
+  function isInView(rec, viewport) {
+      return rec.bottom >= 0 && rec.top <= viewport.getHeight();
   }
 };
 
